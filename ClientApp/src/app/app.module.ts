d0088b158e5149548670bfedfd82385018ecde4a
Yadola <Ya-dola@users.noreply.github.com>
2023-08-15 23:36:21 +0530

    Removed Template Files and Added CSS

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,30 +6,15 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { StudentGridComponent } from './student-grid/student-grid.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    StudentGridComponent,
-  ],
+  declarations: [AppComponent, NavMenuComponent, StudentGridComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'student-grid', component: StudentGridComponent },
-    ]),
+    RouterModule.forRoot([{ path: '', component: StudentGridComponent }]),
   ],
   providers: [],
   bootstrap: [AppComponent],
